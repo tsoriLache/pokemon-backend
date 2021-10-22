a0ea24d32ed4c90b29035977cd4b30b59fa334a2
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-22 16:19:03 +0300

    added pokemon router

--- pokemon-api/src/index.js
+++ pokemon-api/src/index.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 8080;
 const pokemonData = require('./pokemon-data');
 const pokemonRouter = require('./routers/pokemonRouter');
+const userRouter = require('./routers/userRouter');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
+app.use('/',errorHandlerMiddleware);
 app.use('/pokemon', pokemonRouter);
+app.use('/', userRouter);
 
 
 
@@ -17,6 +21,6 @@ app.listen(port, function() {
 });
 
 // route our app
-app.get('/', function(req, res) {
-  res.json('23');
-});
+// app.get('/', function(req, res) {
+//   res.json('23');
+// });
--- pokemon-api/src/routers/pokemonRouter.js
+++ pokemon-api/src/routers/pokemonRouter.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
 const pokemonData = require('../pokemon-data');
+const fs = require('fs');
+const fsp = require('fs/promises');
+const path = require('path');
+const bp = require('body-parser')
+router.use(bp.json())
+router.use(bp.urlencoded({ extended: true }))
+const userMiddleware = require('../middleware/userHandler');
+
 
 const getDataFromPokedex = async (idOrName)=>{
     apiData= await P.getPokemonByName(`${idOrName}`)
@@ -16,9 +24,20 @@ const getDataFromPokedex = async (idOrName)=>{
     return;
 }
 
+router.use('/',userMiddleware);
+
+
+router.get('/', async function (request, response) {
+    const pokemonsArray =[];
+    const files = await fsp.readdir(path.resolve(`users/${username}`));  
+    for(let file of files){
+        const fileData = await fsp.readFile(path.resolve(`users/${username}/${file}`));
+        pokemonsArray.push(JSON.parse(fileData.toString()).pokemon);
+    }
+    response.json(pokemonsArray);
+})
 
 router.get('/query', async function (request, response) {
-    console.log(request.body);
     const name = request.query.name;
     await getDataFromPokedex(name)
     response.json(pokemonData);
@@ -29,6 +48,34 @@ router.get('/get/:id', async function (request, response) {
     const { id } = request.params;
     await getDataFromPokedex(id)
     response.json(pokemonData);
+})
+
+router.put('/catch/:id',function (request, response) {
+    const username = request.headers.username;
+    const { id } = request.params;
+    const pokemonObj = request.body
+    const check = fs.existsSync(path.resolve(`users/${username}/${id}.json`));
+    if(check){
+        response.status(403).send({ message: 'file already exist on the server'});
+    }else{
+        fs.writeFile(path.resolve(`users/${username}/${id}.json`), JSON.stringify(pokemonObj),
+             (err,data)=> {(err)? console.log(err) : console.log(data)});
+        response.send(`${pokemonObj.pokemon.name} pokemon been caught`);
+    }
+})
+
+
+router.delete('/release/:id',function (request, response) {
+    const username = request.headers.username;
+    const { id } = request.params;
+    const check = fs.existsSync(path.resolve(`users/${username}/${id}.json`));
+    if(check){
+        fs.unlink(path.resolve(`users/${username}/${id}.json`),
+             (err) => {(err)? console.log(err) : console.log('path/file.txt was deleted')});
+        response.send(`pokemon been deleted`);
+    }else{
+        response.status(403).send({ message: 'file does not exist on the server'});
+    }
 })
 
 
