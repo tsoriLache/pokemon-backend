dae5942c25ddce9b4fa850419c5978963be76269
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-01 17:36:03 +0200

    t

--- pokemon-api/src/index.js
+++ pokemon-api/src/index.js
@@ -7,9 +7,13 @@ const pokemonRouter = require('./routers/pokemonRouter');
 const userRouter = require('./routers/userRouter');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
 const userMiddleware = require('./middleware/userHandler');
+const path = require("path");
 
 app.use(cors());
-
+app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', function(req, res) { // serve main path as static file
+  res.sendFile(path.resolve('./dist/index.html'))
+});
 app.use('/',errorHandlerMiddleware);
 app.use('/pokemon', userMiddleware ,pokemonRouter);
 app.use('/user',userMiddleware, userRouter);
@@ -18,8 +22,7 @@ app.use('/user',userMiddleware, userRouter);
 
 
 
+app.listen(process.env.PORT || 3000,
+  () => console.log("Server is running..."));
 
-// start the server
-app.listen(port, function() {
-  console.log('app started');
-});
+
