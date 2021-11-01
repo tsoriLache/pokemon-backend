da6b3cf34ae7a45fb16d1ff294efa32aa2af1ebf
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-01 18:24:20 +0200

    CHANGES

--- pokemon-api/index.js
+++ pokemon-api/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require("cors");
-const pokemonData = require('./pokemon-data');
-const pokemonRouter = require('./routers/pokemonRouter');
-const userRouter = require('./routers/userRouter');
-const errorHandlerMiddleware = require('./middleware/errorHandler');
-const userMiddleware = require('./middleware/userHandler');
+const pokemonData = require('./src/pokemon-data');
+const pokemonRouter = require('./src/routers/pokemonRouter');
+const userRouter = require('./src/routers/userRouter');
+const errorHandlerMiddleware = require('./src/middleware/errorHandler');
+const userMiddleware = require('./src/middleware/userHandler');
 const path = require("path");
 
 app.use(cors());
